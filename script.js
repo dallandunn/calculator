bf0657bea543a9738f079142d82567afc21b3c06
Dallan Dunn <dallanpdunn@gmail.com>
2022-02-09 19:00:50 -0500

    Add working number buttons

--- script.js
+++ script.js
@@ -17,3 +17,17 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 };
+
+const display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('.number');
+
+numberButtons.forEach((number) => {
+    number.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = number.textContent;
+        } else {
+            display.textContent += number.textContent;
+        }
+    });
+
+})
