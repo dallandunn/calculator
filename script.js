2674795d7cb629e7ef2795116f6de0f031fb0e39
Dallan Dunn <dallanpdunn@gmail.com>
2022-02-15 09:42:18 -0500

    Added functioning operator buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 };
 
 function subtract(num1, num2) {
@@ -11,6 +11,7 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
+    if (num1 / num2 === Infinity) return "Error";
     return num1 / num2;
 };
 
@@ -21,9 +22,15 @@ function operate(operator, num1, num2) {
 const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.number');
 const clearButton = document.querySelector('#clear')
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('#equals')
+
+let firstNumber = 0;
+let currentOperation;
 
 clearButton.addEventListener('click', () => {
     display.textContent = 0;
+    firstNumber = 0;
 });
 
 numberButtons.forEach((number) => {
@@ -36,3 +43,23 @@ numberButtons.forEach((number) => {
     });
 
 })
+
+operatorButtons.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (firstNumber === 0) {
+            firstNumber = display.textContent;
+            currentOperation = operator.id;
+            display.textContent = 0;
+        } else {
+            firstNumber = operate(window[currentOperation], firstNumber, display.textContent)
+            display.textContent = 0;
+            currentOperation = operator.id;
+        }
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    let output = operate(window[currentOperation], firstNumber, display.textContent);
+    console.log(output);
+    display.textContent = Math.round(output * 100000000) / 100000000;
+});
